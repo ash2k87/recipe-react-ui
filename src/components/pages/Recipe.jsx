d8e4666a403a67734ff1cc0f19c6aeb0665519b2
Ashwin <ash2k87@gmail.com>
2023-07-30 20:32:56 +0530

    cleanup

--- src/components/pages/Recipe.jsx
+++ src/components/pages/Recipe.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Recipe = () => {
   const [recipe, setRecipe] = useState([]);
